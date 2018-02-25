7581397c70e08f1c2428e81641feea43863eda1b
Murat Caliskan <muratcnjid@hotmail.com>
2018-02-25 06:52:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,12 +20,11 @@ function nowServing(katzDeli) {
 var line = [];
 
 function currentLine(katzDeli) {
-  for (i = 0; i < katzDeli.length; i++) {
-  if (katzDeli.length === 0) {
+  for (let i = 0; i < katzDeli.length; i++) {
     line.push(` `+[i+1]+`. `  + katzDeli[i]);
-    return 'The line is currently empty.';
   }
-  else 
-  return `The line is currently: ${katzDeli.length}. ${name}`;
-  }
+  if (katzDeli.length === 0) {
+    return "The line is currently empty.";
+  } else
+  return(`The line is currently:` + line);
 }
