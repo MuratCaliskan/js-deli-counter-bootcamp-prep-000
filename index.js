92ce88cb23b11ee357ba754b30ac2fbd94759c17
Murat Caliskan <muratcnjid@hotmail.com>
2018-02-25 06:16:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
-}
+var katzDeli = [];
 
 function takeANumber(katzDeli, name) {
-+  katzDeli.push(`${name}`);
-+  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
-+}
+  katzDeli.push(name);
+  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`;
+}
