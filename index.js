df4fd0a3110eba04595c52eb0655da5c47d79347
Murat Caliskan <muratcnjid@hotmail.com>
2018-02-25 06:20:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ function takeANumber(katzDeli, name) {
 }
 
 function nowServing(katzDeli) {
-+  let i = 0;
-+  while (i < katzDeli.length) {
-+    i++;
-+  }
-+  if (katzDeli.length === 0) {
-+    return "There is nobody waiting to be served!";
-+  }
-+  else
-+  return (`Currently serving ${katzDeli.shift()}.`);
-+}
+  let i = 0;
+  while (i < katzDeli.length) {
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else
+  return (`Currently serving ${katzDeli.shift()}.`);
+}
